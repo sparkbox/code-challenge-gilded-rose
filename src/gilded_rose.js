e52a67486e34dc07c203449988ea65f4cac75a48
Jon Oliver <jon@heysparkbox.com>
2020-08-31 15:44:23 -0400

    refactor: source code improvements
    Updates the gilded_rose source code to:
    - export functions from the file for easier testing
    - Update `update_quality` function to accept `items`, rather than keeping
    a hardcoded item list in the file. This also increases testability, and
    keeps all of the "bad code" local to the `update_quality` function,
    which is the function that we want to refactor.
    - Add explanatory comments and example usage, using the above hardcoded
    item list as an example

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -1,19 +1,27 @@
-function Item(name, sell_in, quality) {
+// Item constructor. DO NOT MODIFY OR THE GOBLIN WILL EAT YOU!
+export function Item(name, sell_in, quality) {
   this.name = name;
   this.sell_in = sell_in;
   this.quality = quality;
 }
 
-var items = []
+/*
+* Update inventory
+* @param {Item[]} items - an array of Items representing the inventory to be updated
+* Example usage:
 
-items.push(new Item('+5 Dexterity Vest', 10, 20));
-items.push(new Item('Aged Brie', 2, 0));
-items.push(new Item('Elixir of the Mongoose', 5, 7));
-items.push(new Item('Sulfuras, Hand of Ragnaros', 0, 80));
-items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20));
-items.push(new Item('Conjured Mana Cake', 3, 6));
+const items = [
+  new Item('+5 Dexterity Vest', 10, 20),
+  new Item('Aged Brie', 2, 0),
+  new Item('Elixir of the Mongoose', 5, 7),
+  new Item('Sulfuras, Hand of Ragnaros', 0, 80),
+  new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20),
+  new Item('Conjured Mana Cake', 3, 6),
+];
 
-function update_quality() {
+update_quality(items);
+*/
+export function update_quality(items) {
   for (var i = 0; i < items.length; i++) {
     if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
       if (items[i].quality > 0) {
