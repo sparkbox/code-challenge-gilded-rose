b6f4aaff632551ef3ca715316bed3f073889118c
Gary Tempus <gary@heysparkbox.com>
2020-11-22 13:56:52 -0500

    refactor: decompose conditional
    And replace nested conditional with guard clause.
    We can get out quickly when dealing with legendary items.

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -22,18 +22,19 @@ const items = [
 update_quality(items);
 */
 export function update_quality(items) {
+  function isLegendary(i) { return items[i].name === 'Sulfuras, Hand of Ragnaros'; }
+
   function decrementSellIn(i) {
-    if (items[i].name === 'Sulfuras, Hand of Ragnaros') return;
+    if (isLegendary(i)) return;
     items[i].sell_in -= 1;
   }
 
   for (let i = 0; i < items.length; i++) {
     decrementSellIn(i);
+    if (isLegendary(i)) return;
     if (items[i].name !== 'Aged Brie' && items[i].name !== 'Backstage passes to a TAFKAL80ETC concert') {
       if (items[i].quality > 0) {
-        if (items[i].name !== 'Sulfuras, Hand of Ragnaros') {
-          items[i].quality = items[i].quality - 1
-        }
+        items[i].quality = items[i].quality - 1
       }
     } else {
       if (items[i].quality < 50) {
@@ -56,9 +57,7 @@ export function update_quality(items) {
       if (items[i].name !== 'Aged Brie') {
         if (items[i].name !== 'Backstage passes to a TAFKAL80ETC concert') {
           if (items[i].quality > 0) {
-            if (items[i].name !== 'Sulfuras, Hand of Ragnaros') {
-              items[i].quality = items[i].quality - 1
-            }
+            items[i].quality = items[i].quality - 1
           }
         } else {
           items[i].quality = items[i].quality - items[i].quality
