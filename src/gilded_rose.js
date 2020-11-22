d2e8a170d2d46e3916acca1b4b9c7050630c3865
Gary Tempus <gary@heysparkbox.com>
2020-11-22 13:10:28 -0500

    refactor: use string comparison and change var to let

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -22,17 +22,17 @@ const items = [
 update_quality(items);
 */
 export function update_quality(items) {
-  for (var i = 0; i < items.length; i++) {
-    if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+  for (let i = 0; i < items.length; i++) {
+    if (items[i].name !== 'Aged Brie' && items[i].name !== 'Backstage passes to a TAFKAL80ETC concert') {
       if (items[i].quality > 0) {
-        if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
+        if (items[i].name !== 'Sulfuras, Hand of Ragnaros') {
           items[i].quality = items[i].quality - 1
         }
       }
     } else {
       if (items[i].quality < 50) {
         items[i].quality = items[i].quality + 1
-        if (items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+        if (items[i].name === 'Backstage passes to a TAFKAL80ETC concert') {
           if (items[i].sell_in < 11) {
             if (items[i].quality < 50) {
               items[i].quality = items[i].quality + 1
@@ -46,14 +46,14 @@ export function update_quality(items) {
         }
       }
     }
-    if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
+    if (items[i].name !== 'Sulfuras, Hand of Ragnaros') {
       items[i].sell_in = items[i].sell_in - 1;
     }
     if (items[i].sell_in < 0) {
-      if (items[i].name != 'Aged Brie') {
-        if (items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+      if (items[i].name !== 'Aged Brie') {
+        if (items[i].name !== 'Backstage passes to a TAFKAL80ETC concert') {
           if (items[i].quality > 0) {
-            if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
+            if (items[i].name !== 'Sulfuras, Hand of Ragnaros') {
               items[i].quality = items[i].quality - 1
             }
           }
